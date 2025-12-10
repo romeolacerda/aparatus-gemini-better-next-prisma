da40a00bca6739391c2c067c25f949b2e5789b20
Romeo <romeolacerdafarias@gmail.com>
2025-12-10 20:25:50 -0300

    chore: update Next.js configuration to replace experimental server components with serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -10,9 +10,7 @@ const nextConfig: NextConfig = {
       }
     ]
   },
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
-  },
+  serverExternalPackages: ["@prisma/client", "prisma"],
   outputFileTracingIncludes: {
     "/api/**/*": ["./app/generated/prisma/**/*"],
   },
